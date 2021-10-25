18d5843fa3407d3ac2fda0d079b9e9b0bc7424a1
Arik Alexandrov <arikxl@gmail.com>
2021-10-25 16:31:05 +0300

    use toggle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import './App.css';
-
-import useLocalStorage from './components/useLocalStorage';
-import useUpdatedConsoleLog from './components/useUpdatedConsoleLog';
+// import UseToggleComp from './components/useToggle/UseToggleComp';
+// import UseLocalStorageComp from './components/useLocalStorage/UseLocalStorageComp';
+// import useUpdatedConsoleLog from './components/useUpdatedConsoleLog';
 
 
 function App() {
 
-  const [name, setName] = useLocalStorage('name', '')
-  useUpdatedConsoleLog(name)
+  // useUpdatedConsoleLog(name)
 
   return (
     <>
-      <input 
-        type="text" 
-        value={name} 
-        onChange={e => setName(e.target.value)} 
-      />
- 
-
-    </>
+       {/* <UseLocalStorageComp /> */}
+       {/* < UseToggleComp /> */}
+      </>
   );  
 }
 
---
+++ src/components/useLocalStorage/UseLocalStorageComp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import useLocalStorage from './useLocalStorage'
+
+const UseLocalStorageComp = () => {
+
+      const [name, setName] = useLocalStorage('name', '')
+
+    return (
+        <input
+            type="text"
+            value={name}
+            onChange={e => setName(e.target.value)}
+        />
+    )
+}
+
+export default UseLocalStorageComp
